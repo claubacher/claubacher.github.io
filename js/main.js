40854e6ce25ab3f9e1c01809029a96f5c004a914
Chris <claubacher@gmail.com>
2013-11-25 20:48:40 -0800

    Add conditional media query

--- js/main.js
+++ js/main.js
@@ -1,55 +1,56 @@
-var scrollAmount = function() {
-  var dfd = $.Deferred(),
-      prev = pageYOffset;
+if (window.matchMedia("screen and (min-width: 400px)").matches) {
+  var scrollAmount = function() {
+    var dfd = $.Deferred(),
+        prev = pageYOffset;
 
-  setTimeout(function() {
-    dfd.resolve(pageYOffset - prev);
-  }, 100);
+    setTimeout(function() {
+      dfd.resolve(pageYOffset - prev);
+    }, 100);
 
-  return dfd.promise();
-}
-
-var Section = function(el, minTop) {
-  this.$el = $(el);
-  this.height = this.$el.height();
-  this.offset = this.$el.offset().top;
-  this.minTop = minTop;
-}
-
-$(document).ready(function() {
-  var mySections = [];
-  var sections = $('section');
-  for (var i = 0, len = sections.length; i < len; i++) {
-    mySections.push(new Section(sections[i], i * 20));
+    return dfd.promise();
   }
 
-  $(window).on('scroll', function() {
-    for (var i = 0, len = mySections.length; i < len; i++) {
-      var section = mySections[i];
-      if (pageYOffset + section.minTop >= section.offset) {
-        section.$el
-          .addClass('background')
-          .removeAttr('style');
-      }
+  var Section = function(el, minTop) {
+    this.$el = $(el);
+    this.height = this.$el.height();
+    this.offset = this.$el.offset().top;
+    this.minTop = minTop;
+  }
+
+  $(document).ready(function() {
+    var mySections = [];
+    var sections = $('section');
+    for (var i = 0, len = sections.length; i < len; i++) {
+      mySections.push(new Section(sections[i], i * 20));
     }
 
-    scrollAmount().then(function(scrollDiff) {
-      var myBackgrounds = [];
-      for (var i = 0, len = mySections.length; i< len; i++) {
-        if (mySections[i].$el.hasClass('background')) {
-          myBackgrounds.push(mySections[i]);
+    $(window).on('scroll', function() {
+      for (var i = 0, len = mySections.length; i < len; i++) {
+        var section = mySections[i];
+        if (pageYOffset + section.minTop >= section.offset) {
+          section.$el
+            .addClass('background')
+            .removeAttr('style');
         }
       }
 
-      if (scrollDiff < 0 && myBackgrounds.length > 1) {
-        var active = myBackgrounds[myBackgrounds.length - 1];
-        if (pageYOffset + active.minTop <= active.offset) {
-          active.$el
-            .removeClass('background')
-            .css({ top: pageYOffset + active.minTop + 'px' });
+      scrollAmount().then(function(scrollDiff) {
+        var myBackgrounds = [];
+        for (var i = 0, len = mySections.length; i< len; i++) {
+          if (mySections[i].$el.hasClass('background')) {
+            myBackgrounds.push(mySections[i]);
+          }
         }
-      }
+
+        if (scrollDiff < 0 && myBackgrounds.length > 1) {
+          var active = myBackgrounds[myBackgrounds.length - 1];
+          if (pageYOffset + active.minTop <= active.offset) {
+            active.$el
+              .removeClass('background')
+              .css({ top: pageYOffset + active.minTop + 'px' });
+          }
+        }
+      });
     });
   });
-});
-
+}
